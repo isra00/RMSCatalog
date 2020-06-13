99f6a7bd759fce1e0a8576a873509b2c5bac43e8
Isra V <isra00@gmail.com>
2020-06-13 16:41:30 +0300

    Add italian language interface translation and 'whereToFind' template for Mamre Library

---
+++ trans/it.php
@@ -0,0 +1,75 @@
+<?php
+
+return [
+
+	//base.twig
+	'Library Catalog'
+	=> 'Catalogo della Biblioteca',
+
+	'RMS Library catalog'
+	=> 'SRM Catalogo',
+
+	'Search'
+	=> 'Cercare',
+
+	'Search the Catalog'
+	=> 'Libro, autore o categoria',
+
+	//dbManagement.twig
+	'The Database was NOT updated:'
+	=> 'Il Database non è stato aggiornato:',
+
+	'Library Database management'
+	=> 'Gestione del Database della Biblioteca',
+
+	'Current Database file'
+	=> 'Archivio del Database attuale',
+
+	'Cache files <small>(automatically generated)</small>'
+	=> 'Archivi dei cache <small>(generati automaticamente)</small>',
+
+	'Upload new version'
+	=> 'Caricare nuova versione',
+
+	'Warning: uploading a new version will delete the old one!'
+	=> 'Attenzione: caricare una nuova versione eliminerà quella precedente!',
+
+	'Upload'
+	=> 'Caricare',
+
+	//home.twig
+	'Search by title, subtitle or author:'
+	=> 'Cerca per titolo, sottotitolo o autore:',
+
+	'or browse the classification:'
+	=> 'o naviga per classificazione:',
+
+	//record.twig
+	'Year of publication'
+	=> 'Anno di pubblicazione',
+
+	'ISBN'
+	=> 'ISBN',
+
+	'Call n.'
+	=> 'Call n.',
+
+	'Exemplars'
+	=> 'Esemplari',
+
+	'Classification'
+	=> 'Classificazione',
+
+	//searchResults.twig
+	'book(s) in'
+	=> 'libro/i in',
+
+	'class'
+	=> 'classificazione',
+
+	'Found %results% results with “%searchTerm%”'
+	=> 'Si sono trovati %results% risultati con “%searchTerm%”',
+
+
+
+];
