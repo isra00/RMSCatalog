73dceba2d689f4bf463c1b1d7f209757eb50095d
Isra V <isra00@gmail.com>
2020-09-21 16:33:14 +0000

    Create pt.php

---
+++ translations/pt.php
@@ -0,0 +1,174 @@
+<?php
+
+return [
+
+	//base.twig
+	'Library Catalog'
+	=> 'Catálogo da Biblioteca',
+
+	'RMS Library catalog'
+	=> 'Catálogo do SRM',
+
+	'Search'
+	=> 'Pesquisar',
+
+	'Search the Catalog'
+	=> 'Livro, autor ou categoria',
+
+	'At least 3 letters or numbers'
+	=> '3 ou mais letras',
+
+	//dbManagement.twig
+	'The Database was NOT updated:'
+	=> 'A base de dados NÃO foi atualizada:',
+
+	'Library Database management'
+	=> 'Administração da Base de Dados da Biblioteca',
+
+	'Current Database file'
+	=> 'Arquivo da BD atual',
+
+	'Cache files <small>(automatically generated)</small>'
+	=> 'Arquivos de cache <small>(gerados automaticamente)</small>',
+
+	'Upload new version'
+	=> 'Carregar nova versão',
+
+	'Warning: uploading a new version will delete the old one!'
+	=> 'Atenção: carregar uma nova versão eliminará a anterior!',
+
+	'Upload'
+	=> 'Carregar',
+
+	'Google Books data'
+	=> 'Dados de Google Books',
+
+	'Total books in catalog'
+	=> 'Total de livros no catálogo',
+
+	'Books searched in GBooks'
+	=> 'Livros pesquisados em GBooks',
+
+	'Books found in GBooks'
+	=> 'Livros achados em GBooks',
+
+	'Pre-load Google Books data'
+	=> 'Pre-carregar dados extra de Google Books',
+
+	'Click "Warm-up" for fetching extra bibliographic data for every book in the DB. You will need to this if your server will have no Internet access on a regular basis.'
+	=> 'Faça click em "Pre-carregar" para descarregar a informação bibliográfica extra para todos e cada um dos livros. Faça isto se o seridor não vai ter aceso a internet constante, e/ou para fazer a navegação mais rápida',
+	
+	'Warning: this may take A LOT of time, depending on how many books the catalog has.'
+	=> 'Advertência: Pode levar MUITO tempo, dependendo de cuantos livros tem no catálogo.',
+
+	'Warm-up'
+	=> 'Pre-carregar',
+
+	'Loading...'
+	=> 'Carregando...',
+
+	'Loaded for book #'
+	=> 'Descarregado para o livro #',
+	
+	'Finished'
+	=> 'Terminado',
+
+	'RMS Catalog is a non profit-motivated, free and open source software.'
+	=> 'RMS Catalog é um software livre, aberto e sem ânimo de lucro.',
+
+	'E-mail the author'
+	=> 'Escreva ao autor',
+
+	'Source code on GitHub'
+	=> 'Código fonte em GitHub',
+
+	'Not found'
+	=> 'Não encontrado',
+
+	'The cache files have been automatically generated from Excel database because they were not found.'
+	=> 'Os arquivos de cachê foram regenerados automaticamente a partir da base de dados do Excel porque não foram encontrados.',
+
+
+	//home.twig
+	'Search in our %n% books:'
+	=> 'Pesquisa em nossos %n% livros:',
+
+	'Title, subtitle, author or topic'
+	=> 'Procurar por Título, subtítulo, autor ou assunto',
+
+	'or browse the classification:'
+	=> 'ou navegar na classificação:',
+
+	//record.twig
+	'Year of publication:'
+	=> 'Ano da publicação:',
+
+	'ISBN:'
+	=> 'ISBN:',
+
+	'Call n.:'
+	=> 'Call n.:',
+
+	'Exemplars:'
+	=> 'Exemplares:',
+
+	'Classification'
+	=> 'Classificação',
+
+	'<em>(not recorded)</em>'
+	=> '<em>(não registrado)</em>',
+
+	'Loading extra bibliographic data from Internet...'
+	=> 'Carregando dados bibliográficos da Internet...',
+
+	//ajaxExtraData.twig
+	'Automatic book information (might be wrong)'
+	=> 'Informação bibliografica automática (poderia ser incorreta)',
+
+	'Book cover'
+	=> 'Capa',
+
+	'View on Google Books'
+	=> 'Ver em Google Books',
+
+	'Pages:'
+	=> 'Nº páginas:',
+
+	'Size:'
+	=> 'Dimensões:',
+
+	'Publisher:'
+	=> 'Editorial:',
+
+	'Topics:'
+	=> 'Categorias:',
+
+	'ISBN:'
+	=> 'ISBN:',
+
+	'Description:'
+	=> 'Descrição:',
+
+	'No extra data found.'
+	=> 'Não se encontrou informação na Internet.',
+
+	//searchResults.twig
+	'book(s)'
+	=> 'livro(s)',
+
+	'Find all books by this author'
+	=> 'Buscar todos los libros de este autor',
+
+	'class'
+	=> 'categoria',
+
+	'author'
+	=> 'autor',
+
+	'and'
+	=> 'e',
+
+	'Found %results% results with “%searchTerm%”'
+	=> 'Encontrados %results% resultados com “%searchTerm%”',
+
+];
